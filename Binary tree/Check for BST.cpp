c2c25f3a8a48a1488a3229ad9d522c6d01a9bc98
Ananya Jain <65167879+AnanyaJain111@users.noreply.github.com>
2022-04-02 19:52:47 +0530

    Create Check for BST.cpp

---
+++ Binary tree/Check for BST.cpp
@@ -0,0 +1,19 @@
+Node* p=NULL;
+    int final=1;
+    void itree(Node* root){
+        if(root== NULL){
+            return;
+        }
+        itree(root->left);
+        if(p!=NULL && p->data > root->data){
+            final=0;
+        }
+        p=root;
+        itree(root->right);
+    }
+    bool isBST(Node* root) 
+    {
+        // Your code here
+        itree(root);
+        return final;
+    }
